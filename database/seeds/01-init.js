ee4eb9aa5350366365c81605ab4e33e4cafd7d9f
Chris Lien <clien@sharpencx.com>
2020-10-07 10:29:19 -0400

    Seeds and Table Migration

---
+++ database/seeds/01-init.js
@@ -0,0 +1,26 @@
+exports.seed = function(knex) {
+  const departments = [
+    {
+      name: 'Backend',
+      abbreviation: 'BE'
+    },
+    {
+      name: 'Front End',
+      abbreviation: 'UX'
+
+    },
+    {
+      name: 'Dev Ops',
+      abbreviation: 'DO'
+    },
+    {
+      name: 'Quality Assurance',
+      abbreviation: 'QA'
+
+    }
+  ];
+
+  return knex('departments')
+    .insert(departments)
+    .then(() => console.log('Seed data for department added'))
+}
