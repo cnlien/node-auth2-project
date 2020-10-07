88b054dd67d4a67f6a237816e2886e51945a0f7b
Chris Lien <clien@sharpencx.com>
2020-10-07 10:35:44 -0400

    Added users model

---
+++ users/users-model.js
@@ -0,0 +1,34 @@
+const knex = require("../database/connection.js");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+};
+
+function find() {
+  return knex("users")
+    .select("id", "username")
+    .orderBy("id");
+}
+
+function findBy(filter) {
+  return knex("users")
+    .where(filter)
+    .orderBy("id");
+}
+
+async function add(user) {
+  try {
+    const [id] = await knex("users").insert(user, "id");
+    return findById(id);
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+function findById(id) {
+  return knex("users").where({ id }).first();
+}
