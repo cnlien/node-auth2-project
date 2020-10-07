c2c01b4a7f16d8f10fa7b370a34ba31db0088384
Chris Lien <clien@sharpencx.com>
2020-10-07 10:55:31 -0400

    Added register and token gen to auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,11 +4,37 @@ const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
 const Users = require('../users/users-model.js');
-const {
-    isValid
-} = require('../users/users-service.js');
+const { isValid } = require('../users/users-service.js');
 
 router.post('/register', async (req, res, next) => {
+    const credentials = req.body;
+
+    try {
+        if (isValid(credentials)) {
+            const rounds = process.env.BCRYPT_ROUNDS ? parseInt(process.env.BCRYPT_ROUNDS) : 8;
+            const hash = bcryptjs.hashSync(credentials.password, rounds);
+            credentials.password = hash;
+
+            const user = await(Users.add(credentials));
+            const token = generateToken(user);
+            
+            res.status(201).json({
+                data: user, token
+            })
+        } else {
+            next({
+                apiCode: 400,
+                apiMessage: 'Invalid username or password.'
+            })
+        }
+    }
+    catch (err) {
+        next({
+            apiCode: 500, 
+            apiMessage: 'Ther was an error saving the user',
+            error: err
+        })
+    }
 
 })
 
@@ -17,7 +43,17 @@ router.post('/login', async (req, res, next) => {
 })
 
 function generateToken(user) {
-
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        department: user.department
+    }
+    const secret = process.env.JWT_SECRET || 'The secret token'
+    const options = {
+        expiresIn: "1d"
+    }
+    const token = jwt.sign(payload, secret, options);
+    return token;
 }
 
 module.exports = router;
